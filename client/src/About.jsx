61883b0e97df2fcaf12e2e36744c36ce8d54d636
JiriTill <jirik.till@gmail.com>
2025-07-08 08:35:09 +0200

    Create About.jsx

---
+++ client/src/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="max-w-2xl mx-auto p-4 text-white">
+      <h1 className="text-2xl font-bold mb-4">About FantasyTools</h1>
+      <p>
+        FantasyTools is a free fantasy name generator designed to help writers, worldbuilders, and tabletop players generate immersive names for characters, worlds, factions, and more. Created by Neoantica.
+      </p>
+    </div>
+  );
+}
