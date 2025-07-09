61a87640de319761be089af2fd977fc41d8af03d
JiriTill <jirik.till@gmail.com>
2025-07-09 10:51:35 +0200

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,25 +1,18 @@
-// client/src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // We’ll use Firestore
-// import { getAnalytics } from "firebase/analytics"; // optional, only works in browser environments
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtlOqipfWFofMcSwCNgr52BSTfx9V827E",
   authDomain: "fantasytools-a06dd.firebaseapp.com",
   projectId: "fantasytools-a06dd",
-  storageBucket: "fantasytools-a06dd.appspot.com", // fixed typo here
+  storageBucket: "fantasytools-a06dd.appspot.com", // fix typo: should be ".appspot.com"
   messagingSenderId: "311635849788",
   appId: "1:311635849788:web:72b832ab7484f1bb764939",
-  measurementId: "G-2FW4GQGTXX",
+  measurementId: "G-2FW4GQGTXX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Optional: Only works if you're running in a browser with window object
-// const analytics = getAnalytics(app);
-
-// Export Firestore database
-import { getAnalytics } from "firebase/analytics";
-const db = getFirestore(app);
-export default db;
+// 👇 THIS is what was missing
+export const db = getFirestore(app);
