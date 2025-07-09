9afbd9d15b637f0655215a4f57f9aa998189d4bd
JiriTill <jirik.till@gmail.com>
2025-07-09 14:31:32 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Contact from './pages/Contact';
 import Character from './pages/Character';
 import WhyNamesMatter from './pages/WhyNamesMatter';
 import Leaderboard from './pages/Leaderboard';
+import World from './pages/World';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Route path="/character" element={<Character />} />
       <Route path="/why-names-matter" element={<WhyNamesMatter />} />
       <Route path="/leaderboard" element={<Leaderboard />} />
+      <Route path="/world" element={<World />} />
     </Routes>
   );
 }
