a9928849c2696b57e62d6cf6ebb8bb296f8e2e58
Jiri Till <jirik.till@gmail.com>
2025-07-14 14:07:06 +0200

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { inject } from '@vercel/analytics';
+
+// Inject Vercel Analytics
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
