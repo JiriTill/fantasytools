a5761546fd9c60e59ce3bb222f903c296f8b1421
Jiri Till <jirik.till@gmail.com>
2025-08-20 16:42:04 +0200

    Update Faction.jsx

--- client/src/pages/Faction.jsx
+++ client/src/pages/Faction.jsx
@@ -6,6 +6,7 @@ import ShareGeneratedName from '../components/ShareGeneratedName';
 import Footer from '../components/Footer';
 import GoogleAd from '../components/GoogleAd';
 import MultiplexAd from '../components/MultiplexAd';
+import KoFiButton from "../components/KoFiButton";
 
 export default function Faction() {
   const [form, setForm] = useState({
@@ -152,6 +153,7 @@ Rules:
             </div>
           )}
 
+          <KoFiButton id="T6T31JW6G3" />
           <ShareGeneratedName form={form} />
 
                   <Link
