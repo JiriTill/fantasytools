b4c17d9e067408d75a3e07cb4ca97dd6c50517c7
Jiri Till <jirik.till@gmail.com>
2025-08-20 16:42:44 +0200

    Update Religion.jsx

--- client/src/pages/Religion.jsx
+++ client/src/pages/Religion.jsx
@@ -6,6 +6,7 @@ import ShareGeneratedName from '../components/ShareGeneratedName';
 import Footer from '../components/Footer';
 import GoogleAd from '../components/GoogleAd';
 import MultiplexAd from '../components/MultiplexAd';
+import KoFiButton from "../components/KoFiButton";
 
 export default function Religion() {
   const [form, setForm] = useState({
@@ -151,6 +152,7 @@ Rules:
             </div>
           )}
 
+          <KoFiButton id="T6T31JW6G3" />
           <ShareGeneratedName form={form} />
 
                     <Link
